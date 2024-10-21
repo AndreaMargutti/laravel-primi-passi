27e363f119d8aa34f36e0fb636189197f7b7934a
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-21 16:42:44 +0200

    added products page

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $title = 'Hello World!!!';
-    return view('home', compact("title"));
+    return view('home');
+});
+
+Route::get('/products', function () {
+    return view('products');
 });
